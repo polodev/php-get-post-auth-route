691b298fc88d39e60603a74ca457426820cd42f5
Polo Dev <polodev10@gmail.com>
2017-11-08 21:44:43 +0600

    added navigation bar

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,4 +5,27 @@
   <title>PHP starter pack</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css"/>
 </head>
-<body class="bg-info">
+<body class="bg-info pt-5">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" >
+    <a class="navbar-brand" href="/">POLO DEV</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About (auth test)</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
